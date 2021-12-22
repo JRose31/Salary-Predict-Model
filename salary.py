a1f838f6627261b7fdf2c8be2bad56bc6af688f4
JRose31 <jroseborough31@gmail.com>
2021-12-22 13:31:52 -0800

    Trained MLPRegressor

--- salary.py
+++ salary.py
@@ -84,7 +84,59 @@ salary_df['Gender'].isnull().sum()
 salary_df['Gender'] = salary_df[(salary_df['Gender'] == 'Female')|(salary_df['Gender'] == 'Male')]['Gender']
 
 salary_df['Annual Base Salary'].isnull().sum()
-salary_df.shape
-salary_df.dropna(subset=['Annual Base Salary'], inplace=True)
 
+def clean_salary(amount):
+    return int(amount.replace('$', '').replace(',', '').replace(' ', '').replace('CAD', '').replace('()', ''))
+
+for idx, val in enumerate(salary_df['Annual Base Salary']):
+    try:
+        salary_df['Annual Base Salary'].iloc[idx] = clean_salary(val)
+    except:
+        salary_df['Annual Base Salary'].iloc[idx] = np.nan
+        
+salary_df.dropna(subset=['Annual Base Salary'], inplace=True)
+salary_df.shape
 salary_df.info()
+
+# Encoding
+# Ordinal encode degree
+education_dict = {'No Schooling Completed' : 0, 'Some High School, No Diploma' : 1, 'High School Graduate, Diploma or the equivalent (e.g. GED)' : 2,
+                  'Some College credit, no degree' : 3, 'Trade, Technical, Vocational Training' : 4, 'Associate Degree' : 5,
+                  "Bachelor's Degree" : 6, 'Professional Degree' : 7, "Master's Degree" : 8, 'Doctorate Degree' : 9}
+
+salary_df['Highest Level of Education Received'] = salary_df['Highest Level of Education Received'].map(education_dict)
+
+# One hot encode other categorical variables
+age_df = pd.get_dummies(salary_df['Age Range'])
+jobs_df = pd.get_dummies(salary_df['Job Title'])
+genders_df = pd.get_dummies(salary_df['Gender'])
+
+# drop non encoded columns
+salary_df.drop(['Age Range', 'Job Title', 'Gender'], axis=1, inplace=True)
+
+# add all encdoded dfs (including original which now contains continous data) into one
+concat_dfs = [age_df, jobs_df, genders_df, salary_df]
+encoded_df = pd.concat(concat_dfs, axis=1)
+
+# split target and predictors
+target = pd.to_numeric(encoded_df['Annual Base Salary'])
+inputs = encoded_df.drop('Annual Base Salary', axis=1)
+
+
+#impute inputs
+imp_mean = IterativeImputer(random_state=0)
+clean_X = imp_mean.fit_transform(inputs)
+
+clean_inputs = pd.DataFrame(data=clean_X, columns=inputs.columns)
+clean_inputs.isnull().sum()
+
+X = clean_inputs
+y = target
+
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+regr = MLPRegressor(random_state=1, max_iter=500).fit(X_train, y_train)
+regr.predict(X_test[:2])
+print(regr.score(X_test, y_test))
